d916c18ffa0e0d4960b981d47d6b22f69034538e
gracjan grzech <g.grzech@icloud.com>
2024-02-11 21:57:29 +0100

    Database Migration

---
+++ Migrations/20240211205457_Add-Tags.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BracketMaker.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Question_Brackets_QuizId",
+                table: "Question");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Brackets",
+                table: "Brackets");
+
+            migrationBuilder.RenameTable(
+                name: "Brackets",
+                newName: "Quizzes");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Quizzes",
+                table: "Quizzes",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Tag",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 32, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tag", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QuizTag",
+                columns: table => new
+                {
+                    QuizId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    TagId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuizTag", x => new { x.QuizId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_QuizTag_Quizzes_QuizId",
+                        column: x => x.QuizId,
+                        principalTable: "Quizzes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_QuizTag_Tag_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tag",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuizTag_TagId",
+                table: "QuizTag",
+                column: "TagId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Question_Quizzes_QuizId",
+                table: "Question",
+                column: "QuizId",
+                principalTable: "Quizzes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Question_Quizzes_QuizId",
+                table: "Question");
+
+            migrationBuilder.DropTable(
+                name: "QuizTag");
+
+            migrationBuilder.DropTable(
+                name: "Tag");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Quizzes",
+                table: "Quizzes");
+
+            migrationBuilder.RenameTable(
+                name: "Quizzes",
+                newName: "Brackets");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Brackets",
+                table: "Brackets",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Question_Brackets_QuizId",
+                table: "Question",
+                column: "QuizId",
+                principalTable: "Brackets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
