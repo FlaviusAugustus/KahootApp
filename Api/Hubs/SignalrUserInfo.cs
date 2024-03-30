86783e82c8821ddc7da1ad405d6fb0b68f8d0879
gracjan grzech <g.grzech@icloud.com>
2024-03-30 23:54:19 +0100

    Add SignalrUserInfo

---
+++ Api/Hubs/SignalrUserInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace KahootBackend.Hubs;
+
+public class SignalrUserInfo<T> where T : notnull
+{
+    public HashSet<T> Connections { get; } = [];
+    public HashSet<T> Groups { get; } = [];
+
+    public bool IsEmpty => Groups.Count == 0 && Connections.Count == 0;
+}
